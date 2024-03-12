408394934b2222608b9054b7f195c8aaa40a384c
oaplaxis <camjam0312@gmail.com>
2024-03-12 16:16:50 -0400

    added more error pages

--- server/routes/ErrorRoutes.js
+++ server/routes/ErrorRoutes.js
@@ -10,4 +10,16 @@ router.get("/404", (req, res) => {
     res.render("errors/404");
 });
 
+router.get("/401", (req, res) => {
+    res.render("errors/401");
+});
+
+router.get("/403", (req, res) => {
+    res.render("errors/403");
+});
+
+router.get("/500", (req, res) => {
+    res.render("errors/500");
+});
+
 export default router;
